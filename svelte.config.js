0b3a0b1e0ac3bb15c5041acbe520333f7977c87a
trixky <git config --global user.name>
2024-09-22 22:00:48 +0200

    [Env] github action: setup the github action

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const baseDir = process.env.BASE_PATH || "/threejs_journey";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,7 +14,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+		}),
+		paths: {
+			base: baseDir,
+		},
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+// --------------------------------- wasm for rapier
 // wasm and topLevelAwait added for rapier
 // https://github.com/viridia/demo-rapier-three
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
+
+// --------------------------------- deploy on github action
+// https://github.com/ErickKS/vite-deploy
+// https://github.com/sitek94/vite-deploy-demo
+const repo = 'threejs_journey_r3f'
+
 export default defineConfig({
+	base: `/${repo}/`,
 	plugins: [wasm(), topLevelAwait(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
